d1f1e7fcab740f82c60354a1916e5b0efb1d1505
CalRhoads <calrrhoads@gmail.com>
2023-11-02 19:29:42 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-const gifts = ["Teddy Bear","Drone","Doll"];
-function wrapGifts(gifts) {
-    for (let i = 0;i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    debugger
-}
-return gifts;
-}
-wrapGifts(gifts);
-
-
 function writeCards(names,event) {
-    let messages = [];
-    for (let i = 0; i < names.length; i++) {
-        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
-        messages.push(message);
+    let messages = []
+    for (let i = 0; i < names.length; i ++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
     }
-    return messages;
+    return messages
 }
-let names = ["Guadalupe","Ollie","Aki"];
-let event = ["Surprise"];
 
-function countDown(number) {
-    while (number >= 0) {
-        console.log(number);
-        number--;
+function countDown(startingNumber) {
+    while (startingNumber > 0){
+        console.log(startingNumber);
+        startingNumber-=1;
     }
+    console.log(startingNumber)
 }
-writeCards("Guadalupe","Surprise");
